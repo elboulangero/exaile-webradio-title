447bad0c8b73ce5faa1bec01908c19c50199e29a
elboulangero <elboulangero@gmail.com>
2013-11-30 21:49:34 +0100

    Big update for FIP and exaile today
    + Update for current version of exaile
    + Update for current fipradio.fr website
    + Add Nova radio
    + Use json and BeautifulSoup modules to parse data from HTTP request

--- __init__.py
+++ __init__.py
@@ -1,149 +1,131 @@
-import os
-import logging
-import threading
 import time
 import random
 
-import xl
-from xl.nls import gettext as _
-from xl import event, common
+from xl import player, event
 
-from scrap import FIPScrapper
+from scrap import FIPScrapper, NovaScrapper
 
-FREQUENCY = 10  # seconds
+import logging
+logger = logging.getLogger(__name__)
+
+PERIOD = 10  # seconds
 
 _PLUGIN = None
 
-TRACK_CHANGE_CALLBACKS = (
-        'playback_buffering',
-        'playback_player_resume',
-        'playback_current_changed',
-        'playback_track_start',
+PLAYER_START_CALLBACKS = (
         'playback_player_start',
-        'playback_track_end',
-        'player_loaded',
+        'playback_player_resume',
         )
 
-
-logger = logging.getLogger(__name__)
+PLAYER_STOP_CALLBACKS = (
+        'playback_player_end',
+        'playback_player_pause',
+        )
 
 def enable(exaile):
-    if (exaile.loading):
+    if exaile.loading:
         event.add_callback(_enable, 'exaile_loaded')
     else:
         _enable(None, exaile, None)
- 
-def disable(exaile):
-    global _PLUGIN
-    _PLUGIN.stop()
- 
+
 def _enable(eventname, exaile, nothing):
     global _PLUGIN
-    if not _PLUGIN:
-        _PLUGIN = WebRadioTitlePlugin(exaile)
-    _PLUGIN.exaile = exaile
-    for signal in TRACK_CHANGE_CALLBACKS:
-        xl.event.add_callback(_PLUGIN.change, signal)
 
+    _PLUGIN = WebRadioTitlePlugin(exaile)
+
+    for signal in PLAYER_START_CALLBACKS:
+        event.add_callback(_PLUGIN.on_start, signal)
+    for signal in PLAYER_STOP_CALLBACKS:
+        event.add_callback(_PLUGIN.on_stop, signal)
+
+def disable(exaile):
+    global _PLUGIN
+
+    for signal in PLAYER_START_CALLBACKS:
+        event.remove_callback(_PLUGIN.on_start, signal)
+    for signal in PLAYER_STOP_CALLBACKS:
+        event.remove_callback(_PLUGIN.on_stop, signal)
+
+    _PLUGIN.stop()
+ 
 
 
 class WebRadioTitlePlugin(object):
  
     def __init__(self, exaile):
-        self.exaile = exaile
+        logger.debug("")
         self.scrapper = None
-        self._stop = None
-        self._previous = {}
-        self._tmptag = False
 
     def __del__(self):
+        logger.debug("")
         self.stop()
 
-    def change(self,  *args, **kwargs):
-        if self.exaile is None:
-            return
-        if not hasattr(self.exaile, 'player'):
-            logger.debug(_("Player not loaded, ignoring change call"))
-            return
 
-        track = self.exaile.player.current
+
+    def on_start(self, type, object, data):
+        logger.debug("Type: " + type)
+
+        # Stop
+        self.stop()
+
+        # Get track
+        track = player.PLAYER.current
         if not track:
-            logger.debug(_("Player stopped, stop fetching titles"))
-            self.stop()
+            logger.debug("No track")
             return
 
-        if self._tmptag:  # Ignore event while tags set in updatetrack()
+        # Check if track is a webradio
+        if not track.get_type() == "http":
+            logger.debug("Track type is not http")
             return
 
+        # Get URL
         url = track.get_loc_for_io()
         
         # Look for a web scrapper that knows this url
-        matchcls = None
-        for cls in [FIPScrapper]:  # more coming !
+        for cls in [FIPScrapper, NovaScrapper]:
             if cls.match(url):
-                matchcls = cls
-        if matchcls:
-            self.run(matchcls)
-        else:
-            logger.debug(_("Current track does not match any webradio scrapper"))
-            self.stop()
+                self.start(cls, track)
+                return
+        logger.debug("Current track does not match any webradio scrapper")
 
-    def haschanged(self, tags):
-        return self._previous is None or \
-               any(tags.get(k) != self._previous.get(k) for k in tags)
+    def on_stop(self, type, object, data):
+        logger.debug("Type: " + type)
 
-    @common.threaded
-    def run(self, scrappercls):
-        if self.scrapper and isinstance(self.scrapper, scrappercls):
-            # Already running with this scrapper
-            return
-        logger.info(_("Scrapping started"))
-        self.start()
-        self.scrapper = scrappercls()
-        while not self.stopped:
-            try:
-                d = self.scrapper.current()                
-                # Update track if some field changed
-                if self.haschanged(d):
-                    logger.debug(_("New track scrapped %s") % d)
-                    self.updatetrack(d)
-                    self._previous = d
-                time.sleep(FREQUENCY)
-            except Exception, e:
-                logger.exception(e)
-        self.scrapper = None
-        self._previous = None
-        logger.info(_("Scrapping stopped"))
+        # Stop
+        self.stop()
 
-    def start(self):
-        self._stop = threading.Event()
+    def start(self, scrappercls, track):
+        logger.debug("Start fetching titles")
+
+        # Start new scrapper
+        self.scrapper = scrappercls(self, track)
+        self.scrapper.start()
 
     def stop(self):
-        if self._stop:
-            self._stop.set()
+        logger.debug("Stop fetching titles")
 
-    @property
-    def stopped(self):
-        return self._stop.isSet()
+        # Stop current scrapper
+        if self.scrapper:
+            self.scrapper.stop()
+            self.scrapper = None
 
-    def updatetrack(self, data):
-        track = self.exaile.player.current
+    def update_track(self, cause, track, infos):
+        logger.debug("cause: " + cause + ", track: " + str(track) + ", infos: " + str(infos))
+
+        # Ensure a track is defined
         if not track:
+            logger.debug("No track")
             return
-        for tag in ['artist', 'title', 'album', 'author']:
-            value = data.get(tag)
-            if value:
+
+        # Set tags in track
+        for tag in ['artist', 'title', 'album', 'date']:
+            value = infos.get(tag)
+            if value is not None:
                 track.set_tag_raw(tag, value)
-
-        if not data:
-            return
-
-        logger.debug(_("Simulate track change"))
         track.set_tag_raw('__length', random.randint(180, 240))  # fake length
-        loc = track.get_tag_raw('__loc')
-        self._tmptag = True
-        fakepath = os.path.abspath(os.urandom(random.randint(16, 32)))
-        track.set_tag_raw('__loc', fakepath)  # fake is_local() for audioscrobbler plugin
-        event.log_event('playback_track_start', self.exaile.player, track, async=False)
-        track.set_tag_raw('__loc', loc)
-        self._tmptag = False
+
+        # Trigger a notification
+        if cause == 'updated':
+            logger.debug("Simulate track change")
+            event.log_event('playback_track_start', player.PLAYER, track)
--- scrap.py
+++ scrap.py
@@ -1,14 +1,55 @@
-import re
+import threading
 import urllib2
+import json
 
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    BeautifulSoup = None
 
-class WebRadioScrapper(object):
-    def __init__(self, *args, **kwargs):
-        self.uri = kwargs.get('uri')
+import logging
+logger = logging.getLogger(__name__)
+
+class WebRadioScrapper(threading.Thread):
+    def __init__(self, plugin, track):
+        threading.Thread.__init__(self)
+        self._stopevent = threading.Event()
+        self.plugin = plugin
+        self.track = track
+        self.infos = {}
+    
+    def run(self):
+        logger.debug("-> Scrapping " + self.name);
+        while not self._stopevent.isSet():
+            logger.debug("Loop..." + self.name)
+            
+            data = self.download()
+            infos = self.extract(data)
+            infos = self.postprocess(infos)
+
+            if any(infos.get(k) != self.infos.get(k) for k in infos):
+                self.infos = infos
+                self.plugin.update_track('updated', self.track, self.infos)
+
+            self._stopevent.wait(10)
+
+        self.plugin.update_track('stopped', self.track, self.default_infos())
+        logger.debug("-> Done with " + self.name);
+
+    def stop(self):
+        self._stopevent.set()
 
     @classmethod
     def match(cls, fileuri):
         return False
+
+    def default_infos(self):
+        return {
+            'artist' : self.name,
+            'title'  : self.name,
+            'album'  : self.name,
+            'date'   : ""
+            }
 
     def download(self):
         try:
@@ -17,8 +58,8 @@ class WebRadioScrapper(object):
         except:
             return None
 
-    def extract(self, d):
-        return {}
+    def extract(self, data):
+        raise NotImplementedError("Please implement that in the scrapper")
 
     def postprocess(self, infos):
         for k, v in infos.items():
@@ -29,39 +70,99 @@ class WebRadioScrapper(object):
                 v = unicode(v)
             except UnicodeDecodeError:
                 pass
-            infos[k] = v.rstrip().lstrip().title()
+            infos[k] = v.strip().title()
         return infos
 
-    def current(self):
-        d = self.download()
-        c = self.extract(d)
-        c = self.postprocess(c)
-        return c
 
 
 class FIPScrapper(WebRadioScrapper):
     def __init__(self, *args, **kwargs):
         super(FIPScrapper, self).__init__(*args, **kwargs)
-        self.uri = "http://sites.radiofrance.fr/chaines/fip/endirect/"
+        self.name = "FIP Radio"
+        self.uri = "http://www.fipradio.fr/sites/default/files/direct-large.json"
 
     @classmethod
     def match(cls, fileuri):
+        if not BeautifulSoup:
+            raise NotImplementedError('BeautifulSoup is not available.')
+            return False
         return fileuri.startswith("http://mp3.live.tv-radio.com/fip/all/")
 
-    def extract(self, d):
-        main = {}
-        regex = re.compile("<td[^>]*><span class=\"blanc11\">\s*<b>(?P<artist>[^<]*)</b>(\s*\|)(?P<title>[^<\|]*)</span>(?P<extra>.*)</td>", re.IGNORECASE)
-        r = regex.search(d)
-        if r:
-            main = r.groupdict()
-            extra = main.pop('extra')
-            if extra:
-                regex = re.compile("(<br>)?Auteur : (?P<auteur>.*)<br>Album : (?P<album>.*)<Br>", re.IGNORECASE)
-                e = regex.search(extra)
-                main.update(e.groupdict() if e else {})
-        return main
+    def extract(self, data):
+        #logger.debug("data: " + data)
+
+        infos = self.default_infos()
+
+        html = json.loads(data).pop("html")
+        if not html:
+            return infos
+        html.replace('\n', '')
+        #logger.debug("html: " + html)
+
+        soup = BeautifulSoup(html).find(attrs = { "class" : "direct-current" })
+        if not soup:
+            return infos
+        #logger.debug("soup: " + soup.prettify())
+
+        tag_artist = soup.find(attrs = { "class" : "artiste" })
+        if tag_artist:
+            infos['artist'] = str(tag_artist.string).strip()
+
+        tag_title = soup.find(attrs = { "class" : "titre" })
+        if tag_title:
+            infos['title'] = str(tag_title.string).strip()
+
+        tag_album = soup.find(attrs = { "class" : "album" })
+        if tag_album:
+            infos['album'] = str(tag_album.string).strip()
+
+        tag_year = soup.find(attrs = { "class" : "annee" })
+        if tag_year:
+            infos['year'] = str(tag_year.string).strip().strip('()')
+        
+#        logger.debug("artist: " + infos['artist'] +
+#                     ", title: " + infos['title'] +
+#                     ", album: " + infos['album'] +
+#                     ", date: " + infos['year'])
+
+        return infos
 
 
-if __name__ == "__main__":
-    fs = FIPScrapper()
-    print fs.current()
+
+class NovaScrapper(WebRadioScrapper):
+    def __init__(self, *args, **kwargs):
+        super(NovaScrapper, self).__init__(*args, **kwargs)
+        self.name = "Radio Nova"
+        self.uri = "http://www.novaplanet.com/radionova/ontheair"
+
+    @classmethod
+    def match(cls, fileuri):
+        if not BeautifulSoup:
+            raise NotImplementedError('BeautifulSoup is not available.')
+            return False
+        return fileuri.startswith("http://broadcast.infomaniak.net:80/radionova")
+
+    def extract(self, data):
+        infos = self.default_infos()
+        
+        html = json.loads(data).pop("track").pop("markup")
+        if not html:
+            return infos
+        html.replace('\n', '')
+
+        soup = BeautifulSoup(html)
+        if not soup:
+            return infos
+
+        tag_artist = soup.find(attrs = { "class" : "artist" })
+        if tag_artist:
+            artist = tag_artist.string
+            if not artist:
+                artist = tag_artist.find('a').string
+            infos['artist'] = str(artist).strip()
+
+        tag_title = soup.find(attrs = { "class" : "title" })
+        if tag_title:
+            infos['title'] = str(tag_title.string).strip()
+
+        return infos
